6c50479298c973f285f231701a5ab8ec33b63256
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-10 00:36:32 -0300

    Testando aquivos estaticos

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 // setando EJS como engine de renderização de HTML
 app.set('view engine', 'ejs');
 
+// setando local dos aquivos estáticos
+app.use(express.static('public'))
+
 app.get("/:nome/:lang", (req, res) => {
     let nome = req.params.nome;
     let lang = req.params.lang;
