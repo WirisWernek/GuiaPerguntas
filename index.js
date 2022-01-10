e895d4928477c65e4cdbba3e62a7e43f46d5b51f
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-09 23:48:28 -0300

    Passando dados dinamicamente e renderizando no html

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const app = express();
 // setando EJS como engine de renderização de HTML
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/:nome/:lang", (req, res) => {
+    let nome = req.params.nome;
+    let lang = req.params.lang;
+    res.render("index", {
+        nome: nome,
+        lang: lang
+    });
 });
 
 app.listen(5000, () => {
