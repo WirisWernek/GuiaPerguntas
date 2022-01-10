972721fc3d6194aeb0379198ea727e7e7a0dcd29
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-10 00:51:46 -0300

    Removido Testes e adicionado Bootstrap v4.5.3

--- index.js
+++ index.js
@@ -7,32 +7,8 @@ app.set('view engine', 'ejs');
 // setando local dos aquivos estáticos
 app.use(express.static('public'))
 
-app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome;
-    let lang = req.params.lang;
-    let produtos = [{
-            nome: "alface",
-            preco: 1.20
-        },
-        {
-            nome: "colve",
-            preco: 2
-        },
-        {
-            nome: "requeijão",
-            preco: 5
-        },
-        {
-            nome: "refrigerante",
-            preco: 4
-        }
-    ]
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        msg: false,
-        produtos: produtos
-    });
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 app.listen(5000, () => {
