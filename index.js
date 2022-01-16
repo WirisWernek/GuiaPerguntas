48493138534e534f34699787f4044b0526ff2160
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-16 20:50:17 -0300

    Adicionado body-parser para converter as requisiçoes em estruturas js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
 
 // setando EJS como engine de renderização de HTML
@@ -7,12 +8,22 @@ app.set('view engine', 'ejs');
 // setando local dos aquivos estáticos
 app.use(express.static('public'))
 
+// configurando body-parser para converter os dados do formulário em uma estrutura javascript
+app.use(bodyparser.urlencoded({
+    extended: false
+}))
+
+// permitindo body-parser entender json
+app.use(bodyparser.json());
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Formulário Recebido!");
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send("Formulário Recebido! titulo " + titulo + " Descrição: " + descricao);
 });
 
 app.get("/perguntar", (req, res) => {
