21ae25e20e19db93f66303188cfacfad508c96da
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-17 01:02:20 -0300

    Passando dados pra pagina de pergunta

--- index.js
+++ index.js
@@ -66,7 +66,9 @@ app.get("/pergunta/:id", (req, res) => {
         }
     }).then((pergunta) => {
         if (pergunta != undefined) {
-            res.render("pergunta");
+            res.render("pergunta", {
+                pergunta: pergunta
+            });
         } else {
             res.redirect("/");
         }
