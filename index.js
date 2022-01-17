5a1fb9d0b571c7f470a4cd509388c6bb1ae7771d
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-17 06:23:51 -0300

    Adicionada listagem de repostas

--- index.js
+++ index.js
@@ -79,8 +79,18 @@ app.get("/pergunta/:id", (req, res) => {
         }
     }).then((pergunta) => {
         if (pergunta != undefined) {
-            res.render("pergunta", {
-                pergunta: pergunta
+            Resposta.findAll({
+                where: {
+                    perguntaId: pergunta.id
+                },
+                order: [
+                    ["id", "DESC"]
+                ]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
             });
         } else {
             res.redirect("/");
