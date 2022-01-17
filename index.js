4ef0c601d3c3794b9d5ba1f56e595fa4aed3b209
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-17 00:21:46 -0300

    Ordenando perguntas pela mais recente

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ app.use(bodyparser.json());
 // Rotas
 app.get("/", (req, res) => {
     Pergunta.findAll({
-        raw: true
+        raw: true,
+        order: [
+            ["id", "DESC"] // ASC = Crescente || DESC = Decrescente 
+        ]
     }).then(perguntas => {
         res.render("index", {
             perguntas: perguntas
