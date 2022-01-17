90ddbf960db7c4eca6097c93794f795685750c59
Wiris Rafael J Wernek <rafaeljwerneck43@gmail.com>
2022-01-16 23:31:00 -0300

    Adicionado model para criar a tabela pergunta

---
+++ database/Pergunta.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const connection = require("./database");
+
+const Pergunta = connection.define("tb_pergunta", {
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    descricao: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    }
+});
+
+Pergunta.sync({
+    force: false
+}).then(() => {
+    console.log("Tabela Criada")
+});
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const connection = require("./database/database");
+const perguntaModel = require("./database/Pergunta");
 const app = express();
 // Database
 connection
