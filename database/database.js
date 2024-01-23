45e0ab8077d1a07f2aa760853835989d61738532
Wiris Wernek <rafaeljwerneck43@gmail.com>
2024-01-22 23:06:42 -0300

    feat: atualiza versões, ajusta variáveis de ambiente e add env.example

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const connection = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+const connection = new Sequelize(process.env.BANCO, process.env.USUARIO, process.env.SENHA, {
     host: 'localhost',
     dialect: 'mysql'
 });
